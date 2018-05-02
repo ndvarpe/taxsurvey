28348c13def7d606a38016a558bb3f41abce2430
nitin.varpe <nitin.varpe@wolterskluwer.com>
2018-05-02 11:42:30 +0530

    Changes in controller.

--- TaxSurvey/app/survey/question.controller.js
+++ TaxSurvey/app/survey/question.controller.js
@@ -12,19 +12,29 @@
           console.log($scope.question);
           switch ($scope.question.Type) {
               case 'radio':
-                              if ($scope.question.Answer) {
-                                  $state.go('form.question', { questionId: $scope.question.Id + 1 });
+              case 'checkbox':
+                              var selectedAnswers = $scope.question.Options.filter(function (opt) { return opt.Selected == true });
+                              if (selectedAnswers && selectedAnswers.length > 0) {
+                                  if ($scope.question.Id == $scope.totalQuestions.length) {
+                                      console.log($scope.$parent.questions);
+                                  }
+                                  else{
+                                      $state.go('form.question', { questionId: $scope.question.Id + 1 });
+                                  }
                               }
                             break;
               case 'text':
                               if ($scope.question.Answer) {
                                   $state.go('form.question', { questionId: $scope.question.Id + 1 });
                               }
-                            break; 
-              case 'checkbox':
-                              if ($scope.question.Answer) {
+                              if ($scope.question.Id == $scope.totalQuestions.length) {
+                                  console.log($scope.$parent.questions);
+                              }
+                              else {
                                   $state.go('form.question', { questionId: $scope.question.Id + 1 });
                               }
+                              break;
+              default:
                             break;
           }
       }

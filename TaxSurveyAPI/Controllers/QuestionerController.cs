b19f8848472931ef6364c99d031b23d59466ce37
Aman Dharwal <Aman.Dharwal@wolterskluwer.com>
2018-05-02 17:28:46 +0530

    weightagePercent calculated

--- TaxSurveyAPI/Controllers/QuestionerController.cs
+++ TaxSurveyAPI/Controllers/QuestionerController.cs
@@ -22,10 +22,45 @@ namespace TaxSurveyAPI.Controllers
             return questionerFactory.GetQuestioner();
         }
         [HttpPost]
-        public void Post([FromBody]List<Question> questions)
+        public HttpResponseMessage Post([FromBody]List<Question> questions)
         {
-            
+            try
+            {
+                int totalWeightage = 0;
+                int selectedWeightage = 0;
+                foreach (var question in questions)
+                {
+                    totalWeightage = totalWeightage + question.TotalWeightage;
+                    QuestionType type = QuestionType.text;
+                    Enum.TryParse<QuestionType>(question.Type, out type);
+                    switch (type)
+                    {
+                        case QuestionType.checkbox:
+                            var questionOptions = question.Options.Where(o => o.IsSelected == true);
+                            foreach (var item in questionOptions)
+                            {
+                                totalWeightage = totalWeightage + item.Weightage;
+                            }
+                            break;
+                        case QuestionType.radio:
+                            var questionOption = question.Options.First(o => o.Value == question.SelectedOption);
+                            selectedWeightage = selectedWeightage + questionOption.Weightage;
+                            break;
+                        case QuestionType.text:
+                            selectedWeightage = 10;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                int weightagePercent = (selectedWeightage / totalWeightage) * 100;
+                var message = Request.CreateResponse(HttpStatusCode.OK, weightagePercent);
+                return message;
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
         }
-
     }
 }

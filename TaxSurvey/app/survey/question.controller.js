131f52e9ad3f4ff00930581171e164a7ddc4516f
nitin.varpe <nitin.varpe@wolterskluwer.com>
2018-05-02 11:05:33 +0530

    Changes in questions controller.

--- TaxSurvey/app/survey/question.controller.js
+++ TaxSurvey/app/survey/question.controller.js
@@ -3,10 +3,30 @@
 
   angular
     .module('formApp')
-      .controller('questionController', ['$scope', 'questionsFactory', '$stateParams', questionController]);
+      .controller('questionController', ['$scope', 'questionsFactory', '$state', '$stateParams', questionController]);
 
-  function questionController($scope, questionsFactory, $stateParams) {
-      console.log($stateParams.questionId);
+  function questionController($scope, questionsFactory, $state, $stateParams) {
+      $scope.totalQuestions = $scope.$parent.questions;
       $scope.question = $scope.$parent.questions.filter(function (q) { return q.Id == $stateParams.questionId })[0];
+      $scope.submitAnswer = function () {
+          console.log($scope.question);
+          switch ($scope.question.Type) {
+              case 'radio':
+                              if ($scope.question.Answer) {
+                                  $state.go('form.question', { questionId: $scope.question.Id + 1 });
+                              }
+                            break;
+              case 'text':
+                              if ($scope.question.Answer) {
+                                  $state.go('form.question', { questionId: $scope.question.Id + 1 });
+                              }
+                            break; 
+              case 'checkbox':
+                              if ($scope.question.Answer) {
+                                  $state.go('form.question', { questionId: $scope.question.Id + 1 });
+                              }
+                            break;
+          }
+      }
   }
 })();
